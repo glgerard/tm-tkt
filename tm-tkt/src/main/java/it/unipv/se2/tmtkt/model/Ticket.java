ba698f027fb91680aa82eb24d791d076521942e4
Gianluca Gerard <glgerard@gmail.com>
2013-12-19 22:26:12 +0100

    Added first and last event dates in SHOW table.

--- tm-tkt/src/main/java/it/unipv/se2/tmtkt/model/Ticket.java
+++ tm-tkt/src/main/java/it/unipv/se2/tmtkt/model/Ticket.java
@@ -1,5 +1,5 @@
 package it.unipv.se2.tmtkt.model;
-// Generated Dec 19, 2013 3:47:23 PM by Hibernate Tools 3.4.0.CR1
+// Generated Dec 19, 2013 10:08:59 PM by Hibernate Tools 3.4.0.CR1
 
 
 import javax.persistence.AttributeOverride;
